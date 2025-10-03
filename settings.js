d94ddd1709bea9ffaf7398cd99233bfe388f258d
wwwnadunnimsara2007-del <www.nadunnimsara2007@gmail.com>
2025-10-03 14:42:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "THARUSHA-MD&ntRUADyS#DFyt_BBUhK4nU_CW4lWZSXYZCTef8u3PYZLbu3gk_EY",
+    SESSION_ID: process.env.SESSION_ID || "THARUSHA-MD&r1IzCRAK#DkBrKZv0Tgade9JDSmOzBOZo8z6MHkRXbRhoQlnmjpo",
     // add your Session Id make sure it starts with VISPER-MD&
 
     CINESUB_API_KEY: process.env.CINESUB_API_KEY || "e0795fcd-58a9-4c0e-994d-648f146077cb",
